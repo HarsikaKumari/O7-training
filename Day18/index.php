9e71fc67e7774e4f9ac93db867cabf58f28c7fcb
Harsika Kumari <kumariharsika8@gmail.com>
2024-07-02 22:09:01 +0530

    learned variable in php

---
+++ Day18/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = "Harsika";
+$age = 20;
+$course = "php";
+$contact = 785535234;
+$college = "Lovely Professional University";
+
+echo "<h1>".$name. "\nwith age\n". $age. "\n from \n". $college ."\nis assigned to\n". $course. "</h1>";
+?>
